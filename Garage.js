b5cd13681eecc90b58f0d3301056ae2a59399ff4
Atila Suleyman <AtilaSuleyman@hotmail.co.uk>
2017-08-23 22:13:38 +0100

    completed garage and ammended JSON

---
+++ Garage.js
@@ -0,0 +1,61 @@
+let garage = [];
+
+function checkInCar(brand, model, year, regNo, faults){
+    let car = {Brand: brand, Model: model, Year: year, RegNo: regNo, Faults: faults };
+    garage.push(car);
+    console.log(garage.length);
+    garage.forEach(function(element) {
+        console.log(element.Brand);
+        console.log(element.Model);
+        console.log(element.Year);
+        console.log(element.RegNo);
+        console.log(element.Faults);
+    });
+    let paragraph = document.getElementById("p1");
+    paragraph.innerHTML = "";
+    paragraph.innerHTML += `Your ${car.Brand} ${car.Model} has been checked in!`;
+    document.forms.submitForm.reset();
+}
+
+function loadGarage(){
+    let output = document.getElementById("p3");
+    output.innerHTML = "";
+    garage.forEach(function(element) {
+        newParagraph = document.createElement("p");
+        newParagraph.innerHTML += `Brand: ${element.Brand}, Model: ${element.Model}, Year: ${element.Year}, RegNo: ${element.RegNo}`;
+        output.appendChild(newParagraph);
+    });
+}
+
+function removeVehicle(regiNum){
+    let paragraph2 = document.getElementById("p2");
+    paragraph2.innerHTML = "";
+    for(let i = 0; i<garage.length; i++){
+        if(garage[i].RegNo === regiNum){
+            paragraph2.innerHTML += `Brand: ${garage[i].Brand}, Model: ${garage[i].Model}, Year: ${garage[i].Year}, RegNo: ${garage[i].RegNo}`;
+            garage.splice(i, 1);
+        }
+    }
+    document.forms.checkOutForm.reset();
+}
+
+function calculateFees(regiNum){
+    let paragraph4 = document.getElementById("p4");
+    paragraph4.innerHTML = "";
+    let bill =""
+    let statement = ""
+    for(let i = 0; i<garage.length; i++){
+        if(garage[i].RegNo === regiNum){
+            if(garage[i].Faults === 0){
+                statement = "£0, car has no faults";
+                paragraph4.innerHTML += statement;
+            }
+            else{
+                bill ="£"+(garage[i].Faults*100);
+                statement = "Your car has "+garage[i].Faults+" it will therefore cost "+bill+" to fix";
+                paragraph4.innerHTML += statement;
+            }
+        }
+    }
+    document.forms.billsForm.reset();
+}
