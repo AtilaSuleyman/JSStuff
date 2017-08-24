28e821a547be8224616f219ee6df350787070157
Atila Suleyman <AtilaSuleyman@hotmail.co.uk>
2017-08-24 21:17:35 +0100

    added input functionality , people can now  type in their word of choice

--- Strings4.js
+++ Strings4.js
@@ -1,10 +1,13 @@
 function countForTriples(word){
-	var wordToCheck = word.split('');
-	var cntr = 0;
-	for(var i=0; i<wordToCheck.length-2; i++){
+	let wordToCheck = word.split('');
+	let cntr = 0;
+	for(let i=0; i<wordToCheck.length-2; i++){
 		if(wordToCheck[i]===wordToCheck[i+1] && wordToCheck[i]===wordToCheck[i+2]){
 			cntr = cntr + 1;
 		}
 	}
+	let output = document.getElementById("output");
+	output.innerHTML = "";
+	output.innerHTML += cntr;
 	console.log(cntr);
 }
